8eb2ab2d4d32c073ac30880d0d953c7b89619dbf
Caio Teixeira <caio.teixeira@unyleya.com.br>
2023-07-05 16:40:00 -0300

    new features and types

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,16 +1,19 @@
-import { Keyboard, KeyboardAvoidingView, Platform } from 'react-native';
+import React from 'react'
+import {GestureResponderEvent, Keyboard, KeyboardAvoidingView, KeyboardAvoidingViewProps, Platform} from 'react-native';
 
 export type ReactNativeInputKeyboardProps = {
-  children?: any;
-} ;
+  onPress?: (event: GestureResponderEvent) => void
+} & Omit<KeyboardAvoidingViewProps, 'behavior' | 'onStartShouldSetResponder' | 'onResponderGrant'>;
 
-export default function ReactNativeInputKeyboard({ children, ...rest }: ReactNativeInputKeyboardProps) {
+export default function ReactNativeInputKeyboard({children, onPress, ...rest}: ReactNativeInputKeyboardProps) {
   return (
     <KeyboardAvoidingView
-      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
       {...rest}
-      onResponderGrant={Keyboard.dismiss}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
       onStartShouldSetResponder={() => true}
+      onResponderGrant={(e) => {
+        typeof onPress === "function" ? onPress(e) : Keyboard.dismiss()
+      }}
     >
       {children}
     </KeyboardAvoidingView>
