b2f50270274fd92639e3ab06d5d0f3ea43d7be22
Caio Teixeira <caioedut@gmail.com>
2023-08-11 15:28:38 -0300

    refactor: stop using KeyboardAvoidingView with new implementation

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,40 +1,92 @@
-import React from 'react';
-import {
-  GestureResponderEvent,
-  Keyboard,
-  KeyboardAvoidingView,
-  KeyboardAvoidingViewProps,
-  Platform,
-} from 'react-native';
+import React, { useEffect, useRef, useState } from 'react';
+import { Animated, GestureResponderEvent, Keyboard, TextInput, useWindowDimensions, ViewProps } from 'react-native';
 
-export type ReactNativeInputKeyboardProps = {
+import measure from './measure';
+
+const { State: TextInputState } = TextInput;
+
+export type InputKeyboardProps = {
+  enabled?: boolean;
   offset?: number;
   onPress?: (event: GestureResponderEvent) => void;
-} & Omit<
-  KeyboardAvoidingViewProps,
-  'behavior' | 'keyboardVerticalOffset' | 'contentContainerStyle' | 'onStartShouldSetResponder' | 'onResponderGrant'
->;
+} & Omit<ViewProps, 'onStartShouldSetResponder' | 'onResponderGrant'>;
 
-export default function ReactNativeInputKeyboard({
+export default function InputKeyboard({
   enabled = true,
   offset,
   onPress,
+  style,
   children,
   ...rest
-}: ReactNativeInputKeyboardProps) {
+}: InputKeyboardProps) {
+  const dimensions = useWindowDimensions();
+
+  const offsetAnim = useRef(new Animated.Value(0)).current;
+  const [containerY, setContainerY] = useState(0);
+  const [kbHeight, setKbHeight] = useState(0);
+
+  useEffect(() => {
+    if (!enabled) return;
+
+    const showListener = Keyboard.addListener('keyboardDidShow', ({ endCoordinates }) => {
+      setKbHeight(endCoordinates.height);
+    });
+
+    const hideListener = Keyboard.addListener('keyboardDidHide', () => {
+      setKbHeight(0);
+    });
+
+    return () => {
+      showListener.remove();
+      hideListener.remove();
+    };
+  }, [enabled]);
+
+  useEffect(() => {
+    if (!enabled) return;
+
+    (async () => {
+      let newOffsetY = 0;
+
+      const $input = TextInputState.currentlyFocusedInput();
+
+      if ($input) {
+        const { y, height } = await measure($input);
+        const winHeight = dimensions.height - kbHeight;
+        const inputY = y + height;
+
+        if (inputY > winHeight) {
+          newOffsetY = winHeight - containerY - inputY - height - (offset || 0);
+        }
+      }
+
+      Animated.timing(offsetAnim, {
+        toValue: newOffsetY,
+        duration: 200,
+        useNativeDriver: true,
+      }).start();
+    })();
+  }, [dimensions, kbHeight, enabled]);
+
   return (
-    <KeyboardAvoidingView
-      style={{ flex: 1 }}
-      {...rest}
-      enabled={enabled}
-      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-      keyboardVerticalOffset={offset}
-      onStartShouldSetResponder={() => enabled}
+    <Animated.View
+      onLayout={({ nativeEvent }) => {
+        setContainerY(nativeEvent.layout.y);
+      }}
+      onStartShouldSetResponder={() => {
+        return enabled;
+      }}
       onResponderGrant={(e) => {
         typeof onPress === 'function' ? onPress(e) : Keyboard.dismiss();
       }}
+      {...rest}
+      style={[
+        { flex: 1 },
+        ...(Array.isArray(style) ? style : [style]),
+        { transform: [{ translateY: enabled ? offsetAnim : 0 }] },
+      ]}
     >
       {children}
-    </KeyboardAvoidingView>
+    </Animated.View>
   );
 }
---
+++ lib/measure.ts
@@ -0,0 +1,19 @@
+import { Component } from 'react';
+import { NativeMethods } from 'react-native';
+
+export default async function measure($element: Component & NativeMethods): Promise<{
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+}> {
+  return new Promise((resolve, reject) => {
+    if (!$element) {
+      return reject('Unable to find the elementr');
+    }
+
+    $element.measureInWindow((x, y, width, height) => {
+      return resolve({ x, y, width, height });
+    });
+  });
+}
